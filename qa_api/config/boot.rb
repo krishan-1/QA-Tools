8e2ba740706fcb6db9f08643d030c116c8f7065a
zachary <zacharyedgell0800056641@gmail.com>
2020-11-02 13:59:04 -0500

    modifed ip addr

--- qa_api/config/boot.rb
+++ qa_api/config/boot.rb
@@ -3,11 +3,3 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
 
-require 'rails/commands/server'
-module Rails
-  class Server
-    def default_options
-      super.merge(Host: '34.69.49.131', Port: 443)
-    end
-  end
-end
