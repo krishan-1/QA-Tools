d3c84f591eb018babdd147ad19199f94f0b75114
zachary <zacharyedgell0800056641@gmail.com>
2020-11-09 02:49:27 -0500

    added vue and updated the test methods json return

--- qa_api/app/controllers/tests_controller.rb
+++ qa_api/app/controllers/tests_controller.rb
@@ -64,17 +64,15 @@ class TestsController < ApplicationController
     if log_type == 'complete'
       File.read("#{Rails.root}/experience-engine/front/results.json")
     else
-      File.read("#{Rails.root}/experience-engine/front/test/jest/coverage/coverage-summary.json")
+      convert_and_minify(File.read("#{Rails.root}/experience-engine/front/test/jest/coverage/coverage-summary.json"))
     end
   end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_test
     @test = if id?(params[:id])
-              puts 'is id'
               Test.find(params[:id])
             else
-              puts 'is slug'
               Test.where(slug: params[:id])
             end
   end
@@ -83,16 +81,31 @@ class TestsController < ApplicationController
   def id?(param)
     param.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) != nil
   end
+
   # Check if all required parameters exist
   def check_all_params(params, type)
     if type == 'jest'
       if !params['branch'].nil?
-        check_if_branch_exist(params)
+        check_if_name_is_unique(params) if check_if_branch_exist(params) == true
       else
         custom_error('a error has occurred', 'branch parameter is required', 'N/A')
       end
     else
       false
+    end
+  end
+
+  # Checks if name is unique
+  def check_if_name_is_unique(params)
+    if !params['name'].nil?
+      @tests = Test.where(name: params['name'])
+      if @tests.nil?
+        custom_error('a error has occurred', 'the name must me unique', 'N/A')
+      else
+        true
+      end
+    else
+      custom_error('a error has occurred', 'the name parameter is required', 'N/A')
     end
   end
 
@@ -136,6 +149,60 @@ class TestsController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def test_params
-    params.require(:test).permit(:name, :json_complete, :json_summary, :updated_date, :type, :slug)
+    params.require(:test).permit(:name, :json_complete, :json_summary, :slug)
+  end
+
+  def convert_and_minify(json_file)
+    require 'json'
+    json_data = JSON.parse(json_file)
+    locations = { 'total' => json_data['total'] }
+    file_names = {}
+    json_data.each do |data|
+      location_array = data[0].split('/')
+      is_experience_engine_folder = false
+      values_to_delete = []
+      location_array.each do |folder|
+        is_experience_engine_folder = true if folder == 'experience-engine'
+        next unless !is_experience_engine_folder || folder.include?('.')
+
+        values_to_delete.push(folder)
+        next unless folder.include? '.'
+
+        folder_name_index = location_array.index(folder) - 1
+        if file_names.key?(location_array[folder_name_index])
+          file_names[location_array[folder_name_index]].push(folder)
+        else
+          file_names[location_array[folder_name_index]] = [].push(folder)
+        end
+      end
+      values_to_delete.each do |value|
+        location_array.delete(value)
+      end
+      location = ''
+      location_array.each do |folder|
+        location += "#{folder}/"
+      end
+      locations[location] = {} if !locations.key?(location) && location != ''
+    end
+    file_names.each_key do |file_folder|
+      locations.each_key do |folder|
+        folder_split = folder.split('/')
+        next unless folder_split[-1] == file_folder
+
+        json_data.each_key do |key|
+          file_names[file_folder].each do |file|
+            next unless key.split('/')[-1] == file
+
+            locations[folder][file] = {
+              'lines' => json_data[key]['lines'],
+              'functions' => json_data[key]['functions'],
+              'statements' => json_data[key]['statements'],
+              'branches' => json_data[key]['branches']
+            }
+          end
+        end
+      end
+    end
+    locations.to_json
   end
 end
---
+++ qa_api/app/javascript/componets/home.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>HI!!</h1>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+    }
+  }
+}
+</script>
