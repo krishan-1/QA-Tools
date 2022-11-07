6b7602dfab3346f3c66de83d1f1858c6e3c6816c
himanshu <himanshu.khandelwal@sourcedigital.net>
2022-11-07 14:30:21 +0530

    code refractor for assetsUpload

--- Cypress/cypress.config.js
+++ Cypress/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'nu5phk',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
