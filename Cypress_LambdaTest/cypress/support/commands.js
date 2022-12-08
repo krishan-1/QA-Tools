fac0941fd429196c59991fab2bfb740862a5c5f9
YashJain-sourcedigital <yash.jain@sourcedigital.net>
2022-12-08 14:19:40 +0530

    Complete "Assets" E2E Test Code

--- Cypress_LambdaTest/cypress/support/commands.js
+++ Cypress_LambdaTest/cypress/support/commands.js
@@ -1,27 +1,23 @@
-import 'cypress-file-upload'
-Cypress.Commands.addAll(
-{
-    login(email,password){ 
-    cy.visit('https://admin-staging.sourcesync.io/#/login?redirect=%2F')
-    cy.contains('Login').click()
-    cy.get('#username').type(email)
-    cy.get('#password').type(password)
-    cy.get('#kc-login').click()
-    },
+import "cypress-file-upload";
+import "cypress-real-events";
+Cypress.Commands.addAll({
+  login(email, password) {
+    cy.visit("https://admin-staging.sourcesync.io/#/login?redirect=%2F");
+    cy.contains("Login").click();
+    cy.get("#username").type(email);
+    cy.get("#password").type(password);
+    cy.get("#kc-login").click();
+  },
 
-    logout(){
-        cy.contains('Source Digital').click()
-        cy.get('.q-menu > .q-list >  :nth-child(3)').click()
-        cy.wait(5000);
-    },
+  logout() {
+    cy.contains("Source Digital").click();
+    cy.get(".q-menu > .q-list >  :nth-child(3)").click();
+  },
 
-    navigateToTestOrg(orgName){
-        cy.get('.q-header > .q-toolbar > .items-center > button').eq(1).click()
-        cy.contains('Switch Organization').click()
-        cy.contains(orgName).click()
-        cy.wait(5000);
-    }
-})
-
-
-  
+  navigateToTestOrg(orgName) {
+    cy.get(".q-header > .q-toolbar > .items-center > button").eq(1).click();
+    cy.contains("Switch Organization").click();
+    cy.contains(orgName).click();
+    cy.wait(2000);
+  },
+});
